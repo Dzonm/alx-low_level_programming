54419b4005bf0a9a8a4a30247c779222f783bbe2
dzon <dzonm365@gmail.com>
2022-09-19 08:05:26 -0700

    error corrected

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
@@ -10,8 +10,14 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i <= 14; i++)
-			_putchar(i + '0');
+		int n;
+
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar((n / 10) + 48);
+			_putchar((n % 10) + 48);
+		}
 		_putchar('\n');
 	}
 }
