c4c0b687bbff5902cba22f7f426cc8e734bdea50
dzon <dzonm365@gmail.com>
2022-10-13 12:54:29 -0700

    error corrected

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,22 +14,33 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k, l;
 	char *s3;
 
-	s3 = malloc(*s1 + *s2);
+
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	l = i + j + 1;
+	s3 = malloc(l * sizeof(char));
+
+	if (s3 == NULL)
+		return (NULL);
 
 	for (i = 0, l = 0; s1[i] != '\0'; i++, l++)
-	{
-		if (s1[i] == NULL)
-			s3[l] = " ";
 		s3[l] = s1[i];
-	}
+
 	for (l = l, j = 0; s2[j] != '\0'; l++, j++)
-	{
-		if (s1[j] == NULL)
-			s3[l] = " ";
 		s3[l] = s2[j];
-	}
-	if (s3 != NULL)
-		return (s3);
-	else
-		return (NULL);
+	s3[l] = '\0';
+
+	return (s3);
 }
